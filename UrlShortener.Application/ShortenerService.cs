8342e996223a2e8993a417c67c0ff3d2cb7c7b46
picolino <webmaster.pico@gmail.com>
2020-07-13 19:19:55 +0300

    Create services with required API

---
+++ UrlShortener.Application/ShortenerService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UrlShortener.Application
+{
+    public class ShortenerService
+    {
+        public string GetShortenUrl(string sourceUrl)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetSourceUrlByShortenUrl(string shortUrl)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UrlShortener.Application/StatisticService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UrlShortener.Application
+{
+    public class StatisticService
+    {
+        public void GetShortUrlsDataAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetShortUrlsDataByUser(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
